195b943d04d13c95a6fa84f466081d0e243a4427
Naveen Mahantesh <naveen.mahantesh@glassbeam.com>
2025-07-17 07:21:07 +0530

    updateed
    Signed-off-by: Naveen Mahantesh <naveen.mahantesh@glassbeam.com>

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var express = require('../../');
+var express = require('express');
 
 var app = module.exports = express()
 
@@ -10,6 +10,6 @@ app.get('/', function(req, res){
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
+  app.listen(8080);
+  console.log('Express started on port 8080');
 }
