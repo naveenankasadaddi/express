edd177bb00aaa7ab11a93ec0db6dab56f0b150a6
Naveen <65064825+naveenankasadaddi@users.noreply.github.com>
2025-07-18 19:32:34 +0530

    Update index.js
    update the web app

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello Naveen');
+  res.send('Hello Virat cricket');
 });
 
 /* istanbul ignore next */
